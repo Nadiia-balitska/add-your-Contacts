e60690d9b3f538eca0749de44819c2270c89881c
Nadiia Balitska <161743763+Nadiia-balitska@users.noreply.github.com>
2024-07-03 00:46:36 +0200

    add title

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -13,11 +13,7 @@ export const Home = () => {
       <div className="hero-content text-neutral-content text-center">
         <div className="max-w-md">
           <h1 className="mb-5 text-5xl font-bold">Hello there</h1>
-          <p className="mb-5">
-            Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda
-            excepturi exercitationem quasi. In deleniti eaque aut repudiandae et
-            a id nisi.
-          </p>
+          <p className="mb-5">Here are yours contacts list...</p>
           <Link to="/contacts" className="btn btn-primary">
             Get Started
           </Link>
